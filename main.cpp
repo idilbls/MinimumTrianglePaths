b9d51d4482e974b77b0cc6140cee325c03e57ae8
İdil <idilblks@gmail.com>
2020-10-12 23:38:17 +0300

    file operations added

--- main.cpp
+++ main.cpp
@@ -21,3 +21,38 @@ int minTriPath(vector<vector<int> >& Tri)
     // return the top element. 
     return result[0]; 
 } 
+
+int main() 
+{ 	//The size of the triangle and variables for each element entered
+	int num,size;
+    vector<vector<int> > Tri(size);
+    
+    cout<<"Enter the size of tirangle:";
+    cin>>size;
+    
+    //Numbers are taken from the user.
+    for(int i=0;i<=size;i++)
+    {
+    	//A temporary variable is created to use numbers.
+    	vector<int> temp;
+    	for(int j=0; j<=i ; j++)
+    	{
+    		cin>>num;
+    		temp.push_back(num);
+		}
+    	Tri.push_back(temp);
+    	temp.clear();
+	}	
+	//A file is created for the numbers.
+	std::ofstream outFile("triangle.txt");
+	//The numbers received from the user are printed to the file.
+    for (int i = 0; i < Tri.size(); i++) 
+	{ 
+        for (int j = 0; j < Tri[i].size(); j++) 
+            outFile <<Tri[i][j] << " "; 
+        outFile << endl; 
+    } 
+	
+    cout <<"Minimal path is: "<< minTriPath(Tri); 
+    return 0; 
+} 
